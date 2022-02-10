c7eb8bc9362396d83d8e1d4363b493802b735d2b
albertosanchezc <97926173+albertosanchezc@users.noreply.github.com>
2022-02-10 15:37:12 -0600

    Deployment de la app

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,11 @@
 <?php
 
-$db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'] , $_ENV['DB_PASS'] , $_ENV['DB_BD']);
+$db = mysqli_connect(
+    $_ENV['DB_HOST'],
+    $_ENV['DB_USER'],
+    $_ENV['DB_PASS'] ?? '',
+    $_ENV['DB_BD']
+);
 $db->set_charset('utf8');    
 
 
@@ -10,4 +15,6 @@ if (!$db) {
     echo "errno de depuración: " . mysqli_connect_errno();
     echo "error de depuración: " . mysqli_connect_error();
     exit;
+}else{
+    echo "Conectado";
 }
