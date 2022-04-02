97e7ee2806934be8e3ff1c1588a1216d3ac03883
albertosanchezc <97926173+albertosanchezc@users.noreply.github.com>
2022-04-02 04:14:27 -0600

    sdsfd

--- classes/Email.php
+++ classes/Email.php
@@ -21,33 +21,27 @@ class Email
 
     public function enviarConfirmacion()
     {
-        // Crear el objeto de Email
         $mail = new PHPMailer();
         $mail->isSMTP();
-        $mail->Host = 'smtp.sendgrid.net';
+        $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Port = 587;
+        $mail->Port = 2525;
         $mail->Username = 'e8f93059c33c8b';
         $mail->Password = '340e2e67a23d41';
-
-        $mail->setFrom('cuentas@LashesBar.com');
+        $mail->setFrom($_POST['email']);
         $mail->addAddress('cuentas@LashesBar.com', 'LashesBar.com');
         $mail->Subject = 'Confirma tu Cuenta';
-
         // Set HTML
         $mail->isHTML(TRUE);
         $mail->CharSet = 'UTF-8';
-
         $contenido = "<html>";
-        $contenido .= "<p><strong>Hola ". $this->email . "</strong> Has creado tu cuenta en Lashes Bar, solo debes confirmarla presionando el siguiente enlace</p>";
-        $contenido .= "<p>Presiona aquí: <a href='http://". $_SERVER["HTTP_HOST"] . "/confirmar-cuenta?token=".$this->token."'>Confirmar Cuenta</a> </p>";
+        $contenido .= "<p><strong>Hola " . $this->email . "</strong> Has creado tu cuenta en Lashes Bar, solo debes confirmarla presionando el siguiente enlace</p>";
+        $contenido .= "<p>Presiona aquí: <a href='http://" . $_SERVER["HTTP_HOST"] . "/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a> </p>";
         $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar este correo</p>";
         $contenido .= "</html>";
         $mail->Body = $contenido;
-        
-
         // Enviar el mail
-        $mail->send();
+$mail->send();
     }
 
     public function enviarInstrucciones(){
