19459780bff3262437121ed35aede2835981e320
albertosanchezc <97926173+albertosanchezc@users.noreply.github.com>
2022-04-02 03:06:25 -0600

    prueba correo

--- classes/Email.php
+++ classes/Email.php
@@ -2,8 +2,7 @@
 
 namespace Classes;
 
-
-use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\PHPMAiler;
 
 
 class Email
@@ -26,59 +25,56 @@ class Email
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Port = 587;
+        $mail->Port = 2525;
         $mail->Username = 'e8f93059c33c8b';
         $mail->Password = '340e2e67a23d41';
 
-        $mail->setFrom('albertosanchezc98@gmail.com');
-        $mail->addAddress($_POST['email'], $_POST['nombre']);
+        $mail->setFrom('cuentas@LashesBar.com');
+        $mail->addAddress('cuentas@LashesBar.com', 'LashesBar.com');
         $mail->Subject = 'Confirma tu Cuenta';
-    
-        
 
         // Set HTML
         $mail->isHTML(TRUE);
         $mail->CharSet = 'UTF-8';
 
         $contenido = "<html>";
-        $contenido .= "<p><strong>Hola " . $this->email . "</strong> Has creado tu cuenta en Lashes Bar, solo debes confirmarla presionando el siguiente enlace</p>";
-        $contenido .= "<p>Presiona aquí: <a href='http://" . $_SERVER["HTTP_HOST"] . "/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a> </p>";
+        $contenido .= "<p><strong>Hola ". $this->email . "</strong> Has creado tu cuenta en Lashes Bar, solo debes confirmarla presionando el siguiente enlace</p>";
+        $contenido .= "<p>Presiona aquí: <a href='http://". $_SERVER["HTTP_HOST"] . "/confirmar-cuenta?token=".$this->token."'>Confirmar Cuenta</a> </p>";
         $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar este correo</p>";
         $contenido .= "</html>";
         $mail->Body = $contenido;
-
+        
 
         // Enviar el mail
         $mail->send();
     }
 
-    public function enviarInstrucciones()
-    {
-        // Crear el objeto de Email
-        $mail = new PHPMailer();
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Port = 587;
-        $mail->Username = 'albertosanchezc98@gmail.com';
-        $mail->Password = 'blqvuhfqngqbmdss';
-
-        $mail->setFrom($_POST['email']);
-        $mail->addAddress('cuentas@LashesBar.com', 'LashesBar.com');
-        $mail->Subject = 'Reestablece Tu Password';
-
-        // Set HTML
-        $mail->isHTML(TRUE);
-        $mail->CharSet = 'UTF-8';
-
-        $contenido = "<html>";
-        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has Solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo</p>";
-        $contenido .= "<p>Presiona Aquí:<a href='http://" . $_SERVER["HTTP_HOST"] . "/recuperar?token=" . $this->token . "'>Reestablece Tu Password</a></p>";
-        $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar este correo</p>";
-        $contenido .= "</html>";
-        $mail->Body = $contenido;
-
-        // Enviar el mail
-        $mail->send();
+    public function enviarInstrucciones(){
+                // Crear el objeto de Email
+                $mail = new PHPMailer();
+                $mail->isSMTP();
+                $mail->Host = 'smtp.gmail.com';
+                $mail->SMTPAuth = true;
+                $mail->Port = 587;
+                $mail->Username = 'albertosanchezc98@gmail.com';
+                $mail->Password = 'blqvuhfqngqbmdss';
+        
+                $mail->setFrom('cuentas@LashesBar.com');
+                $mail->addAddress($_POST['email'], $_POST['nombre']);
+                $mail->Subject = 'Reestablece Tu Password';
+        
+                // Set HTML
+                $mail->isHTML(TRUE);
+                $mail->CharSet = 'UTF-8';
+        
+                $contenido = "<html>";
+                $contenido .= "<p><strong>Hola ". $this->nombre . "</strong> Has Solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo</p>";
+                $contenido .= "<p>Presiona Aquí:<a href='http://". $_SERVER["HTTP_HOST"] . "/recuperar?token=" . $this->token . "'>Reestablece Tu Password</a></p>";
+                $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar este correo</p>";
+                $contenido .= "</html>";
+                $mail->Body = $contenido;
+                
+                // Enviar el mail
+                $mail->send();
     }
 }
